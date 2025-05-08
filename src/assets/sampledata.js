83ddbf46a20a08a6ab486ca181e385e66cfa8d71
hasintha <hasinthadarshana3@gmail.com>
2025-05-08 17:24:42 +0530

    create admin product page

---
+++ src/assets/sampledata.js
@@ -0,0 +1,116 @@
+export const sampledata = [
+    {
+        "_id": "67f3936f6e09452ac5b3ce3b",
+        "productId": "COS001",
+        "name": "Hydrating Face Serumhasi",
+        "altNames": [
+            "Face Moisturizer",
+            "Serum huak"
+        ],
+        "description": "A lightweight hejke hydrating serum that keeps your skin fresh all day.",
+        "images": [
+            "https://example.com/images/serum1.jpg"
+        ],
+        "labeledPrice": 3200,
+        "price": 290,
+        "stock": 10,
+        "category": "Skincare",
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f3950e6e09452ac5b3ce3d",
+        "productId": "COS002",
+        "name": "Matte Liquid Lipstick",
+        "altNames": [
+            "Lip Color",
+            "Lip Paint"
+        ],
+        "description": "Long-lasting matte lipstick available in vibrant shades.",
+        "images": [
+            "https://example.com/images/lipstick1.jpg"
+        ],
+        "labeledPrice": 1500,
+        "price": 1299,
+        "stock": 80,
+        "category": "Makeup",
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f3950e6e09452ac5b3ce3d",
+        "productId": "COS002z",
+        "name": "Matte Liquid Lipstick",
+        "altNames": [
+            "Lip Color",
+            "Lip Paint"
+        ],
+        "description": "Long-lasting matte lipstick available in vibrant shades.",
+        "images": [
+            "https://example.com/images/lipstick1.jpg"
+        ],
+        "labeledPrice": 1500,
+        "price": 1299,
+        "stock": 80,
+        "category": "Makeup",
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f3950e6e09452ac5b3ce3d",
+        "productId": "COS002zz",
+        "name": "Matte Liquid Lipstick",
+        "altNames": [
+            "Lip Color",
+            "Lip Paint"
+        ],
+        "description": "Long-lasting matte lipstick available in vibrant shades.",
+        "images": [
+            "https://example.com/images/lipstick1.jpg"
+        ],
+        "labeledPrice": 1500,
+        "price": 1299,
+        "stock": 80,
+        "category": "Makeup",
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f3950e6e09452ac5b3ce3d",
+        "productId": "COS002zzz",
+        "name": "Matte Liquid Lipstick",
+        "altNames": [
+            "Lip Color",
+            "Lip Paint"
+        ],
+        "description": "Long-lasting matte lipstick available in vibrant shades.",
+        "images": [
+            "https://example.com/images/lipstick1.jpg"
+        ],
+        "labeledPrice": 1500,
+        "price": 1299,
+        "stock": 80,
+        "category": "Makeup",
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "67f3950e6e09452ac5b3ce3d",
+        "productId": "COS002y",
+        "name": "Matte Liquid Lipstick",
+        "altNames": [
+            "Lip Color",
+            "Lip Paint"
+        ],
+        "description": "Long-lasting matte lipstick available in vibrant shades.",
+        "images": [
+            "https://example.com/images/lipstick1.jpg"
+        ],
+        "labeledPrice": 1500,
+        "price": 1299,
+        "stock": 80,
+        "category": "Makeup",
+        "isAvailable": true,
+        "__v": 0
+    },
+]
--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -1,5 +1,53 @@
+import axios from "axios"
+import { sampledata } from "../../assets/sampledata"
+import { useState } from "react"
+import { useEffect } from "react"
+
 export default function AdminProductPage() {
+    const [products, setProducts] = useState(sampledata)
+    useEffect(
+        ()=>{
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products").then((res)=>{
+                console.log(res.data)
+                setProducts(res.data)
+            })
+        }, []);
+
+    
+
     return(
-        <h1>Admin Product Page</h1>
+        
+        <div className="w-full h-full max-h-full overflow-y-scroll">
+            <table className="w-full text-center ">
+                <thead>
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Name</th>
+                        <th>Image</th>
+                        <th>Labelled Price</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                  {
+                    products.map((item,index)=>{
+                        return(
+                            <tr key={index}>
+                                <td>{item.productId}</td>
+                                <td>{item.name}</td>
+                                <td><img src={item.images[0]} className="w-[50px] h-[50px]"/></td>
+                                <td>{item.labeledPrice}</td>
+                                <td>{item.price}</td>
+                                <td>{item.stock}</td>
+                            </tr>
+                        )
+                    })
+                  }
+                  
+                </tbody>
+            </table>
+        </div>
     )
 } 
