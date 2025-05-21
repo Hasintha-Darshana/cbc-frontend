a0f5deee15672651ce19f73b05a1a8b6fa5e050d
hasintha <hasinthadarshana3@gmail.com>
2025-05-21 13:00:29 +0530

    create product overview page

---
+++ src/pages/client/productOverview.jsx
@@ -0,0 +1,38 @@
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+import { useParams } from "react-router-dom"
+
+export default function ProductOverviewPage() {
+    const params = useParams()
+    const productId = params.id
+    const [status, setStatus] = useState("loading")//loading, success, error
+
+    const[product, setProduct] = useState(null)
+
+    useEffect(
+       ()=>{
+        axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products/"+productId).then(
+                (response)=>{
+                    console.log(response.data)
+                    setProduct(response.data)
+                    setStatus("success")
+                }
+            ).catch(
+                (err)=>{
+                    console.log(err.response.data)
+                    setStatus("error")
+                    toast.error("Error fetching product details.")
+                }
+            )
+           
+        }
+       
+    ,[])
+    return (
+        <div>
+            This is the product overview page {productId}
+        </div>
+    )
+}
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -35,7 +35,7 @@ export default function LoginPage(){
     return (
         <div className="w-full h-screen bg-[url('/login.jpg')] bg-center bg-cover flex justify-evenly items-center">
             <div className="w-[50%] h-full"></div>  
-            <div className="w-[50%] h-full flex justify-center items-center">
+            <div className="w-[50%] h-full flex justify-center items-center ">
                 <div className="w-[500px] h-[500px] backdrop-blur-md shadow-xl rounded-[20px] flex flex-col justify-center items-center">
                     <input onChange={(e)=>{
                         setEmail(e.target.value)
