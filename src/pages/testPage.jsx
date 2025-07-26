831e86020749869f1a645e1c20504eae41cf373b
hasintha <hasinthadarshana3@gmail.com>
2025-07-26 12:21:27 +0530

    edit order modal status

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,30 +1,9 @@
 
-import {useState}from "react";
-import mediaUpload from "../utils/mediaUpload";
-
 export default function TestPage() {
-    const[image, setImage] = useState(null);
-
-    function imageUpload(){
-       mediaUpload(image).then(
-        (res)=>{
-            console.log(res)
-        }
-       ).catch(
-         (res)=>{
-            console.log(res)
-         }
-       )
-    }
+   
    
     return(
         <div className="w-full h-screen flex items-center justify-center ">
-           <input type = "file" className="file-input file-input-bordered w-full max-w-xs" onChange={
-            (e)=>{
-                setImage(e.target.files[0]);
-            }
-           }></input>
-            <button onClick={imageUpload} className="font-bold bg-green-500 text-white py-2 px-4 rounded">Upload</button>
         </div>
     )
 }
